fa5c4f87e2bccb8fadd479dff00611d422b52e28
dreamev5031 <bbhlin3@gmail.com>
2026-07-26 20:39:40 +0900

    연습 뷰포트 진단 컴포넌트 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import PracticeViewportDiagnostics from './components/PracticeViewportDiagnostics'
 import './styles.css'
 import './practice.css'
 import './practice-landscape.css'
@@ -8,5 +9,6 @@ import './practice-landscape.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <PracticeViewportDiagnostics />
   </StrictMode>,
 )
